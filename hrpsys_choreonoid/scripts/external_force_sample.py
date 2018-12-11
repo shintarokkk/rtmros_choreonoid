1df39bc144d20aa37a5913401e9ec52b462cc9d3
Shintaro Komatsu <komatsu@jsk.imi.i.u-tokyo.ac.jp>
2018-12-11 22:12:31 +0900

    custom external_force.py

--- hrpsys_choreonoid/scripts/external_force_sample.py
+++ hrpsys_choreonoid/scripts/external_force_sample.py
@@ -1,12 +1,15 @@
 import cnoid.Base
 import cnoid.BodyPlugin
+import numpy as np
 
 def addExternalForce():
     robotname = "JAXON_RED"
-    linkname = "WAIST"
+    linkname = "RARM_JOINT7"
     pos   = [0, 0, 0.2] ## link local position
-    force = [100, 0, 0] ## [ N ]
-    tm = 1 ## [ sec ]
+    rarot = np.matrix(np.array([[0.022978, -0.934776, -0.354494], [0.945668, -0.094707, 0.311034], [-0.32432, -0.342381, 0.881812]]))
+    force = np.dot(rarot, np.array([-20, 0, 0])) ## [ N ]
+    force = np.ravel(force)
+    tm = 2 ## [ sec ]
 
     robotItem = cnoid.Base.RootItem.instance().findItem(robotname)
     simulatorItem = cnoid.BodyPlugin.SimulatorItem.findActiveSimulatorItemFor(robotItem)
